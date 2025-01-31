8e9f2b3c649018e93015e434a2d9015de6da8d57
alonso mendoza <alonsomendoza75@gmail.com>
2025-01-30 21:01:14 -0500

    subida de plantillas

---
+++ frontend_angular/angular/vristo-angular-starter/src/app/components/atencion-ciudadano/atencion-ciudadano.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
+import { AppService } from 'src/app/service/app.service';
+
+@Component({
+    selector: 'app-atencion-ciudadano',
+    templateUrl: './atencion-ciudadano.component.html',
+    styleUrl: './atencion-ciudadano.component.css',
+})
+export class AtencionCiudadanoComponent {
+    
+}
---
+++ sistema_integral_api/sistema_integral_api/Controllers/CitizenAttentionControllers/CitizenAttentionController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace sistema_integral_api.Controllers.CitizenAttentionController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitizenAttentionController : ControllerBase
+    {
+        // GET: api/<CitizenAttentionController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CitizenAttentionController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CitizenAttentionController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CitizenAttentionController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CitizenAttentionController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
