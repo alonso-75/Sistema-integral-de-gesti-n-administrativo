83bedf37b9e3f3dd681200c8f50cb8149dc960fc
joel mendoza <joeljosemendozap@gmail.com>
2025-02-19 23:35:07 -0500

    ok

--- frontend_angular/angular/vristo-angular-main/src/app/apps/PQR/pqr-form/pqr-form.component.ts
+++ frontend_angular/angular/vristo-angular-main/src/app/apps/PQR/pqr-form/pqr-form.component.ts
@@ -11,39 +11,78 @@ import { FlatpickrDefaultsInterface } from 'angularx-flatpickr';
     templateUrl: './pqr-form.component.html',
 })
 export class PqrFormComponent {
+    tiposolicitud: any[] = [
+        '',
+        'ACCION DE TUTELA',
+        'PERMISO',
+        'DERECHO DE PETICION',
+        'CERTIFICADO',
+        'INFORMACION',
+        'INSIDENTE DESACATO',
+        'QUERELLA',
+        'FALLO',
+        'PROCESOS JUDICIALES',
+    ];
+    dependecias: any[] = [
+        'JURIDICA',
+        'CONTOL INTERNO',
+        'DESPACHO DELALACALDE',
+        'GESTION SOCIAL',
+        'SECRETARIA DE GOBIERNO',
+        'SECRETARIA DE HACIENDA',
+        'SECRETARIA DE PLANEACION',
+        'SECRETARIA DE SALUD',
+        'SECRETARIA GENERAL',
+        'SEC-GOB-COMISARIA',
+        'SEC-GOB-DISCAPACIDAD',
+        'SEC-GOB-FAMILIAS EN ACCION',
+        'SEC-GOB-GESTION DE RIESGO',
+        'SEC-GOB-INSPECCION',
+        'SEC-GOB-VICTIMAS',
+        'SEC-GOB-DEPORTE',
+        'SEC-GOB-ICBF',
+        'SEC-GOB-ADULTO MAYOR',
+        'SEC-GOB-SISBEN',
+        'SEC-GOB-TRANSITO',
+        'SEC-GOB-CONTABILIDAD',
+        'SEC-GOB-PRESUPUESTO',
+        'SEC-GOB-TESORERIA',
+        'SEC-GOB-UMATA',
+        'SEC-GOB-ARCHIVO',
+        'SEC-GOB-CONTRATACION',
+        'SEC-GOB-TALENTO HUMANO',
+
+
+
+
+
+        
+    ];
     codeArr: any = [];
-        basic: FlatpickrDefaultsInterface;
-    
-    toggleCode = (name: string) => {
-        if (this.codeArr.includes(name)) {
-            this.codeArr = this.codeArr.filter((d: string) => d != name);
-        } else {
-            this.codeArr.push(name);
-        }
-    };
+    basic: FlatpickrDefaultsInterface;
     pqrForm: FormGroup<any>;
     form1!: FormGroup;
-    onSubmit() {
-        throw new Error('Method not implemented.');
-    }
+    input6: any[];
+
     pqr: Omit<Pqr, 'id' | 'fecha'> = {
         tipo: '',
         descripcion: '',
     };
 
-    constructor(private pqrService: PqrService, private fb:FormBuilder) {
-
-      this.basic = {
-        dateFormat: 'Y-m-d',
-        // position: this.store.rtlClass === 'rtl' ? 'auto right' : 'auto left',
-        monthSelectorType: 'dropdown',
-    };
+    constructor(
+        private pqrService: PqrService,
+        private fb: FormBuilder,
+    ) {
+        this.basic = {
+            dateFormat: 'Y-m-d',
+            // position: this.store.rtlClass === 'rtl' ? 'auto right' : 'auto left',
+            monthSelectorType: 'dropdown',
+        };
     }
     ngOnInit() {
-
-      this.form1 = this.fb.group({
-        date1: ['2022-07-05'],
-    });
+        this.form1 = this.fb.group({
+            date1: ['2022-07-05'],
+        });
         // single image upload
         new FileUploadWithPreview('myFirstImage', {
             images: {
@@ -51,6 +90,9 @@ export class PqrFormComponent {
                 backgroundImage: '',
             },
         });
+    }
+    onSubmit() {
+        throw new Error('Method not implemented.');
     }
 
     submitPqr() {
